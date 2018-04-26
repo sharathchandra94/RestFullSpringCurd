6daa7eecf50e6ce678e583f667f08fa2a779c060
40312 <40312@EBIII044F0.corp.hexaware.com>
2018-04-26 10:33:34 +0530

    Servlet Container 3.0 mapping

--- src/main/java/com/hexarest/config/InitilizationClass.java
+++ src/main/java/com/hexarest/config/InitilizationClass.java
@@ -1,9 +1,23 @@
 package com.hexarest.config;
 
-public class InitilizationClass {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-	
-	
-	
-	
+public class InitilizationClass extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { Config.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
 }
