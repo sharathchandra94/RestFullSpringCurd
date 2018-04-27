412c4cc2994e5a7ac3d4dd90f30f36d009fb4624
40312 <40312@EBIII044F0.corp.hexaware.com>
2018-04-27 09:09:08 +0530

    Student Dao and Studnet model Added

--- src/main/java/com/hexarest/control/RestControllerSpring.java
+++ src/main/java/com/hexarest/control/RestControllerSpring.java
@@ -2,23 +2,12 @@ package com.hexarest.control;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
 import com.hexarest.da.Employeedao;
 import com.hexarest.mod.Employee;
 
@@ -34,7 +23,6 @@ public class RestControllerSpring {
 		return output;
 
 	}
-
 	@RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
 	public @ResponseBody Employee getEmployeeById(@PathVariable("id") Integer id) {
 
@@ -42,9 +30,7 @@ public class RestControllerSpring {
 	//	String json = new Gson().toJson(output);
 
 		return output;
-
 	}
-
 	@RequestMapping(value = "/employees", method = RequestMethod.POST)
 
 	public Employee createEmployee(Employee emp) {
